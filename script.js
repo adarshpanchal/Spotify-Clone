0f141288406773c0317ff4af172d8a053f09fdad
Adarsh Panchal <adarshpanchal047@gmail.com>
2026-04-15 18:41:36 +0530

    Library Section implemented

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ topselect.forEach(e => {
         e.classList.add("active")
     })
 });
+
+const buttons=document.querySelectorAll(".librarylist")
+buttons.forEach(e => {
+    e.addEventListener("click",()=>{
+        buttons.forEach(li=>li.classList.remove("active"))
+        e.classList.add("active")
+    })
+});
